d6e106570dbc44f87c784122ad4256777cde29ba
egbakou <egbakoukodjolaurent@gmail.com>
2020-06-11 00:44:26 +0000

    Added fonts and stated Updated SeatingView

--- FurnitureShop3d/FurnitureShop3d/App.xaml.cs
+++ FurnitureShop3d/FurnitureShop3d/App.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using FurnitureShop3d.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
+[assembly: ExportFont("NunitoSans-Regular.ttf", Alias = "ThemeFontRegular")]
+[assembly: ExportFont("NunitoSans-SemiBold.ttf", Alias = "ThemeFontMedium")]
+[assembly: ExportFont("NunitoSans-Bold.ttf", Alias = "ThemeFontBold")]
 namespace FurnitureShop3d
 {
     public partial class App : Application
@@ -10,7 +15,7 @@ namespace FurnitureShop3d
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new SeatingView());
         }
 
         protected override void OnStart()
---
+++ FurnitureShop3d/FurnitureShop3d/Views/SeatingView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FurnitureShop3d.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SeatingView : ContentPage
+    {
+        public SeatingView()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        public List<PropertyType> PropertyTypeList => GetPropertyTypes();
+
+        private List<PropertyType> GetPropertyTypes()
+        {
+            return new List<PropertyType>
+            {
+                new PropertyType { TypeName = "All" },
+                new PropertyType { TypeName = "Office chairs" },
+                new PropertyType { TypeName = "Reclainers" },
+                new PropertyType { TypeName = "Loungue chairs" },
+                new PropertyType { TypeName = "Office" }
+            };
+        }
+
+        private void SelectType(object sender, EventArgs e)
+        {
+            var view = sender as View;
+            var parent = view.Parent as StackLayout;
+
+            foreach (var child in parent.Children)
+            {
+                VisualStateManager.GoToState(child, "Normal");
+                ChangeTextColor(child, "#707070");
+            }
+
+            VisualStateManager.GoToState(view, "Selected");
+            ChangeTextColor(view, "#FFFFFF");
+        }
+
+        private void ChangeTextColor(View child, string hexColor)
+        {
+            var txtCtrl = child.FindByName<Label>("PropertyTypeName");
+
+            if (txtCtrl != null)
+                txtCtrl.TextColor = Color.FromHex(hexColor);
+        }
+    }
+
+    public class PropertyType
+    {
+        public string TypeName { get; set; }
+    }
+
+}
